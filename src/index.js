0690149f5b52c4bbc23ab029edd39ea2e88abc16
zhengyulong-cn <zhengyulong.cn@outlook.com>
2022-12-04 20:17:47 +0800

    feat: 管理资源完成

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import RefImage from './toRef和toRefs.svg';
+import Data from './assets/data.xml';
+import Notes from './assets/data.csv';
 function component() {
   const el = document.createElement('div');
   el.innerHTML = _.join(['Hello', 'webpack'], '-');
+  el.classList.add('hello');
+
+  const myPic = new Image();
+  myPic.src = RefImage;
+  el.appendChild(myPic)
+  // 加载数据
+  console.log('xml', Data);
+  console.log('csv', Notes);
   return el;
 }
 document.body.appendChild(component())
